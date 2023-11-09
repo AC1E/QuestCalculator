cad0303ae1ec31bfee673a466ac5af03c98aac37
Shubam <sammannavar9@gmail.com>
2023-11-09 16:35:43 +0530

    Spacing changes

--- Pilot-FrameWork/Items/ZApplication.cs
+++ Pilot-FrameWork/Items/ZApplication.cs
@@ -12,40 +12,39 @@ namespace Pilot_FrameWork.Items
     public class ZApplication
     {
 
-        
-            private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
-            public static Application application;
-            public static void LaunchApplication(string appName)
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+        public static Application application;
+        public static void LaunchApplication(string appName)
+        {
+            try
             {
-                try
-                {
-                    Logger.Info($"Application '{appName}' launched successfully.");
-                    var appPath = Configuration.Path(appName);
-                    application = Application.Launch(appPath);
+                Logger.Info($"Application '{appName}' launched successfully.");
+                var appPath = Configuration.Path(appName);
+                application = Application.Launch(appPath);
                     
-                }
-                catch (Exception ex)
-                {
-                    Logger.Error($"Error launching application '{appName}': {ex.Message}");
-                }
             }
-            public static ZWindow GetApplicationWindow(string title = "")
+            catch (Exception ex)
             {
-                try
-                {
-                    var windowTitle = string.IsNullOrEmpty(title) ? "Calculator" : title;
-                    var window = application.GetWindow(windowTitle);
-                    return new ZWindow(window);
-                }
-                catch (Exception ex)
-                {
-                    Logger.Error($"Error getting the application window: {ex.Message}");
-                    return null;
-                }
+                Logger.Error($"Error launching application '{appName}': {ex.Message}");
             }
         }
-
-
-
+        public static ZWindow GetApplicationWindow(string title = "")
+        {
+            try
+            {
+                var windowTitle = string.IsNullOrEmpty(title) ? "Calculator" : title;
+                var window = application.GetWindow(windowTitle);
+                return new ZWindow(window);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"Error getting the application window: {ex.Message}");
+                return null;
+            }
+        }
     }
+
+
+
+}
        
